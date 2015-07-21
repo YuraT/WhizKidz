6ab76914eb677fde9955f9c2f0cd241223113fb6
YuraT <ytatisch@gmail.com>
2015-07-21 16:48:51 -0700

    array record

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,10 +1,47 @@
-var main = function() {
+var main = function () {
     var usNum = "";
+    var numbers = [];
+    var oper = [];
     $(".standard button").click(function() {
-        var number = $(this).text();
-        usNum = usNum + number;
+        var input = $(this).text();
+        
+        if (["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."].indexOf(input) !== -1) {
+            console.log("number clicked");
+            usNum = usNum + input;
+        }
+        else if (input === "+") {
+            console.log("+ sign clicked");
+            numbers.push(parseFloat(usNum));
+            usNum = "";
+            oper.push(input);
+        }
+        else if (input === "-") {
+            console.log("- sign clicked");
+            numbers.push(parseFloat(usNum));
+            usNum = "";
+            oper.push(input);
+        }
+        else if (input === "*") {
+            console.log("* sign clicked");
+            numbers.push(parseFloat(usNum));
+            usNum = "";
+            oper.push(input);
+        }
+        else if (input === "/") {
+            console.log("/ sign clicked");
+            numbers.push(parseFloat(usNum));
+            usNum = "";
+            oper.push(input);
+        }
+        else if (input === "=") {
+            console.log("= sign clicked");
+            
+        }
+        
         console.log(usNum);
+        console.log(numbers);
+        console.log(oper);
     });
 };
 
-$(document).ready(main)
+$(document).ready(main);
