ebd62ebe3a4d063cc2898ce815347d67c4ba70b4
YuraT <ytatisch@gmail.com>
2015-07-22 15:44:51 -0700

    Shorten The Calculator Code

--- JS/calculator.js
+++ JS/calculator.js
@@ -2,42 +2,28 @@ var main = function () {
     var usNum = "";
     var numbers = [];
     var oper = [];
-    $(".standard button").click(function() {
+    var all = [];
+    $(".standard button").click(function () {
         var input = $(this).text();
-        
+
         if (["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."].indexOf(input) !== -1) {
             console.log("number clicked");
             usNum = usNum + input;
+            all = all + usNum;
         }
-        else if (input === "+") {
-            console.log("+ sign clicked");
+        else if (["+", "-", "*", "/"] !== -1) {
+            console.log(input + " sign clicked");
             numbers.push(parseFloat(usNum));
             usNum = "";
             oper.push(input);
-        }
-        else if (input === "-") {
-            console.log("- sign clicked");
-            numbers.push(parseFloat(usNum));
-            usNum = "";
-            oper.push(input);
-        }
-        else if (input === "*") {
-            console.log("* sign clicked");
-            numbers.push(parseFloat(usNum));
-            usNum = "";
-            oper.push(input);
-        }
-        else if (input === "/") {
-            console.log("/ sign clicked");
-            numbers.push(parseFloat(usNum));
-            usNum = "";
-            oper.push(input);
+            all = usNum + input;
+            console.log(all);
         }
         else if (input === "=") {
-            console.log("= sign clicked");
-            
+            console.log(input + " sign clicked");
+
         }
-        
+
         console.log(usNum);
         console.log(numbers);
         console.log(oper);
