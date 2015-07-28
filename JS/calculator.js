54dce70980b1b1930902f638b4b63621ae1041e2
YuraT <ytatisch@gmail.com>
2015-07-28 14:46:55 -0700

    Calculator

--- JS/calculator.js
+++ JS/calculator.js
@@ -2,22 +2,18 @@ var main = function () {
     var usNum = "";
     var numbers = [];
     var oper = [];
-    var all = [];
     $(".standard button").click(function () {
         var input = $(this).text();
 
         if (["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."].indexOf(input) !== -1) {
             console.log("number clicked");
             usNum = usNum + input;
-            all = all + usNum;
         }
         else if (["+", "-", "*", "/"] !== -1) {
             console.log(input + " sign clicked");
             numbers.push(parseFloat(usNum));
             usNum = "";
             oper.push(input);
-            all = usNum + input;
-            console.log(all);
         }
         else if (input === "=") {
             console.log(input + " sign clicked");
